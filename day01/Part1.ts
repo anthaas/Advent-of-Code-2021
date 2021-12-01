d9920d65986f7b8788f0e6fa6956cf40763f3e11
Antonin Haas <tondahaas@gmail.com>
2021-12-01 22:00:19 +0100

    remove unnecessary map

--- day01/Part1.ts
+++ day01/Part1.ts
@@ -1,5 +1,5 @@
 import {readFileSync} from 'fs';
 
 const input = readFileSync('./input.txt', 'utf-8').toString().split("\n").map((item) => parseInt(item));
-const increasedCount = input.map((e, i) => e < input.slice(1)[i]).filter(Boolean).length;
+const increasedCount = input.filter((e, i) => e < input.slice(1)[i]).length;
 console.log(increasedCount);
