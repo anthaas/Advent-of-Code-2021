74530d89c6bf3a48568b1c0eb2ebdcf682229c12
Antonin Haas <tondahaas@gmail.com>
2021-12-06 19:32:29 +0100

    use array instead of map

--- day06/Part2.ts
+++ day06/Part2.ts
@@ -1,31 +1,22 @@
 import {readFileSync} from 'fs';
 
 let input = readFileSync('./input.txt', 'utf-8').toString().split(",").map(Number)
-let fishAges = initMap()
-input.forEach(item => fishAges.set(item, Number(fishAges.get(item)) + 1))
-
+let fishAges = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+input.forEach(item => fishAges[item] = fishAges[item] + 1)
 
 for (let i = 0; i < 256; i++) {
-    const nextGen = initMap()
-    for (let j = 8; j >= 0; j--) {
-        if (j == 0) {
-            nextGen.set(8, Number(fishAges.get(0)))
-            nextGen.set(6, Number(nextGen.get(6)) + Number(fishAges.get(0)))
+    const nextGen = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for (let index = 8; index >= 0; index--) {
+        if (index == 0) {
+            nextGen[8] = fishAges[0]
+            nextGen[6] = fishAges[0] + nextGen[6]
         } else {
-            nextGen.set(j - 1, Number(nextGen.get(j - 1)) + Number(fishAges.get(j)))
+            nextGen[index - 1] = fishAges[index] + nextGen[index - 1]
         }
     }
     fishAges = nextGen
 }
 
-function initMap() {
-    const newMap = new Map<number, number>()
-    for (let i = 0; i <= 8; i++) {
-        newMap.set(i, 0)
-    }
-    return newMap
-}
-
-const result = Array.from(fishAges.values()).reduce((sum, current) => sum + current, 0)
+const result = fishAges.reduce((sum, current) => sum + current, 0)
 console.log(result)
 
